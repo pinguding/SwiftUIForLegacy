f3ed06a6e21d2b6899091d59db7e2fa38ab9df66
박종우 <bagjong-u@bagjong-uui-MacBookAir.local>
2023-12-25 22:27:36 +0900

    Start Project

---
+++ SwiftUIForLegacy/SwiftUIForLegacy.h
@@ -0,0 +1,18 @@
+//
+//  SwiftUIForLegacy.h
+//  SwiftUIForLegacy
+//
+//  Created by 박종우 on 12/25/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftUIForLegacy.
+FOUNDATION_EXPORT double SwiftUIForLegacyVersionNumber;
+
+//! Project version string for SwiftUIForLegacy.
+FOUNDATION_EXPORT const unsigned char SwiftUIForLegacyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftUIForLegacy/PublicHeader.h>
+
+
